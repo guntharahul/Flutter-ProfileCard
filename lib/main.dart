212f9c732d4678ef2f8e49475d7ae8d718d497b5
R@HUL <31180528+guntharahul@users.noreply.github.com>
2020-02-28 17:44:52 +0530

    Adding and using external fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,46 @@ import 'package:flutter/material.dart';
 
 //the main function is the starting point for all out flutter apps
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(image: AssetImage('images/diamond_image1.jpg')),
-        ),
-      ),
-    ),
-  );
+          appBar: AppBar(
+            title: new Text("Profile Card"),
+          ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/diamond_image1.jpg'),
+                ),
+                Text(
+                  'Rahul',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Acme'),
+                ),
+                Text(
+                  'I am an android developer',
+                  style: TextStyle(
+                      color: Colors.red.shade300,
+                      fontFamily: 'Acme',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5.0),
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
